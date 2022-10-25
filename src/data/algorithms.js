ebe95a4cd33f428ecf1b935711955d2e58edac9c
neglux <akcaaytuna@gmail.com>
2022-10-25 06:43:36 +0300

    adding algorithm data

---
+++ src/data/algorithms.js
@@ -0,0 +1,34 @@
+const data = [57, 17, 34, 21, 25, 44, 10, 13];
+
+export default [
+  {
+    id: 1,
+    name: "bubblesort",
+    title: "BubbleSort",
+    data,
+  },
+  {
+    id: 2,
+    name: "selectionsort",
+    title: "SelectionSort",
+    data,
+  },
+  {
+    id: 3,
+    name: "insertionsort",
+    title: "InsertionSort",
+    data,
+  },
+  {
+    id: 4,
+    name: "mergesort",
+    title: "MergeSort",
+    data,
+  },
+  {
+    id: 5,
+    name: "quicksort",
+    title: "QuickSort",
+    data,
+  },
+];
