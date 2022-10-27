35fd40b13e1d00c3daba02c8fb1443e12f86fd3b
neglux <akcaaytuna@gmail.com>
2022-10-27 12:54:43 +0300

    removing data

--- src/data/algorithms.js
+++ src/data/algorithms.js
@@ -1,39 +1,32 @@
 import { bubbleSort, insertionSort, selectionSort } from "./codes";
 
-const data = [32, 57, 17, 34, 25, 44, 30, 23, 40, 15, 62];
-
 export default [
   {
     id: 1,
     name: "bubblesort",
     title: "BubbleSort",
-    data: [...data],
     run: bubbleSort,
   },
   {
     id: 2,
     name: "selectionsort",
     title: "SelectionSort",
-    data: [...data],
     run: selectionSort,
   },
   {
     id: 3,
     name: "insertionsort",
     title: "InsertionSort",
-    data: [...data],
     run: insertionSort,
   },
   {
     id: 4,
     name: "mergesort",
     title: "MergeSort",
-    data: [...data],
   },
   {
     id: 5,
     name: "quicksort",
     title: "QuickSort",
-    data: [...data],
   },
 ];
