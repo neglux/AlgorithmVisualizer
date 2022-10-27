599658b0357d29d943ac7541f3a427f9bdc9f698
neglux <akcaaytuna@gmail.com>
2022-10-27 12:26:46 +0300

    stop animator when button clicked, content changed

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -6,23 +6,37 @@ import listView from "./views/listView";
 import algorithms from "../data/algorithms";
 
 import { Animator } from "./helpers/animator";
+import { sleep } from "./helpers/sleep";
 
-const controlContent = (name) => {
+const animator = new Animator(contentView);
+const controlContent = async (name) => {
+  if (animator.isRunning) {
+    animator.stop();
+    await sleep(500); // make sure rendering clicked algorithm after animator stopped
+  }
+
   const algorithm = algorithms.find((algo) => algo.name === name);
   if (!algorithm) return;
 
   model.state.algorithm = algorithm;
+  model.state.algorithm.data = [...model.state.initialData];
+
   contentView.render(model.state.algorithm);
 };
 
 const controlStartAnimation = () => {
-  const animator = new Animator(contentView);
   animator.animate(model.state.algorithm);
+};
+
+const controlResetAnimation = () => {
+  animator.stop();
+  model.state.algorithm.data = [...model.state.initialData];
 };
 
 function App() {
   contentView.addHashChangeHandler(controlContent);
   contentView.addStartHandler(controlStartAnimation);
+  contentView.addResetHandler(controlResetAnimation);
   listView.render(algorithms);
 }
 
