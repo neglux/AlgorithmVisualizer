92b01f96f13fc4415f444f16c6c5e6d4cafd456c
neglux <akcaaytuna@gmail.com>
2022-10-27 12:24:39 +0300

    handling animator running state

--- src/data/codes.js
+++ src/data/codes.js
@@ -1,4 +1,5 @@
 export async function bubbleSort(array, animator) {
+  animator.start();
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = 0; j < array.length - 1 - i; j++) {
       await animator.setCur(j);
@@ -10,6 +11,7 @@ export async function bubbleSort(array, animator) {
       }
     }
   }
+  animator.stop();
   return array;
 }
 
@@ -19,6 +21,7 @@ export async function selectionSort(arr, animator) {
     array[index1] = array[index2];
     array[index2] = temp;
   };
+  animator.start();
   for (let i = 0; i < arr.length; i++) {
     let smallest = { index: i, value: arr[i] };
     for (let j = i; j < arr.length; j++) {
@@ -30,6 +33,7 @@ export async function selectionSort(arr, animator) {
       await animator.setSwap(i, smallest.index);
     }
   }
+  animator.stop();
   return arr;
 }
 
@@ -39,6 +43,7 @@ export async function insertionSort(arr, animator) {
     array[index1] = array[index2];
     array[index2] = temp;
   };
+  animator.start();
   for (let i = 1; i < arr.length; i++) {
     let cur = arr[i];
     for (let j = i - 1; j >= 0; j--) {
@@ -46,8 +51,9 @@ export async function insertionSort(arr, animator) {
       if (cur < arr[j]) {
         swap(arr, j, j + 1);
         await animator.setSwap(j, j + 1);
-      }
+      } else break;
     }
   }
+  animator.stop();
   return arr;
 }
--- src/scripts/helpers/animator.js
+++ src/scripts/helpers/animator.js
@@ -2,8 +2,9 @@ import { addStyle } from "./addStyle";
 import { sleep } from "./sleep";
 
 export class Animator {
-  #SPEED = 1 * 1000; // 1000ms => 1s
+  #SPEED = 0.7 * 1000; // 1000ms => 1s
   #algo;
+  isRunning;
 
   constructor(view) {
     this.view = view;
@@ -11,8 +12,15 @@ export class Animator {
 
   animate(algo) {
     this.#algo = algo;
-
     this.#algo.run(this.#algo.data, this);
+  }
+
+  start() {
+    this.isRunning = true;
+  }
+
+  stop() {
+    this.isRunning = false;
   }
 
   #findItem(index) {
@@ -22,18 +30,22 @@ export class Animator {
   }
 
   async setCur(index) {
-    const item = this.#findItem(index);
-    addStyle(item, "background-color", "#f0f0f0");
-    await sleep(this.#SPEED);
-    this.view.render(this.#algo);
+    if (this.isRunning) {
+      const item = this.#findItem(index);
+      addStyle(item, "background-color", "#f0f0f0");
+      await sleep(this.#SPEED);
+      this.view.update(this.#algo);
+    }
   }
 
   async setSwap(index1, index2) {
-    const item1 = this.#findItem(index1);
-    const item2 = this.#findItem(index2);
-    addStyle(item1, "background-color", "#b3ffae");
-    addStyle(item2, "background-color", "#b3ffae");
-    await sleep(this.#SPEED);
-    this.view.render(this.#algo);
+    if (this.isRunning) {
+      const item1 = this.#findItem(index1);
+      const item2 = this.#findItem(index2);
+      addStyle(item1, "background-color", "#b3ffae");
+      addStyle(item2, "background-color", "#b3ffae");
+      await sleep(this.#SPEED);
+      this.view.update(this.#algo);
+    }
   }
 }
